190fd736628d55c8a11449ffab9df025f14b88ec
hieu.ho <hotuankn1@gmail.com>
2023-09-28 10:27:41 +0700

    app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { dataSourceOptions } from 'db/data-source';
+
 
 @Module({
-  imports: [UsersModule, CategoriesModule, ProductsModule, OrdersModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UsersModule,
+    CategoriesModule,
+    ProductsModule,
+    OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
