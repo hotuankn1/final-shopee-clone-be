2475167f2c0bbe1041126c100e37209d86f1e459
hieu.ho <hotuankn1@gmail.com>
2023-09-28 10:29:52 +0700

    user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post, Session } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(
+        private readonly usersService: UsersService,
+        private authService: AuthService,
+    ) { }
+
+    @Post('/signup')
+    async signup(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.usersService.create(body.email, body.password);
+        console.log(session);
+        console.log(user);
+        session.userId = user.id;
+        return user
+    }
+
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
+    }
+
+    @Get('/:id')
+    async findUser(@Param('id') id: string) {
+        const user = await this.usersService.findOne(parseInt(id));
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+        return user;
+    }
+
+}
