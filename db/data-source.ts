16f2e2ba571a9b64a48f65396320143884ac25b4
hieu.ho <hotuankn1@gmail.com>
2023-09-28 10:28:05 +0700

    app configuration

---
+++ db/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+
+
+export const dataSourceOptions: DataSourceOptions = {
+    type:'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: 'hotuan123',
+    database:'final-shopee-clone',
+    entities: ['./dist/src/**/*.entity.{ts,js}'],
+    migrations:['./dist/db/migrations/*.{ts,js}']
+}
+
+const dataSource = new DataSource(dataSourceOptions)
+export default dataSource
