f274e8ceadc8b4e80e5af65c1efbbe5d4b61ddbb
hieu.ho <hotuankn1@gmail.com>
2023-10-02 17:25:41 +0700

    category API

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+    constructor(
+        private readonly categoriesService: CategoriesService,
+    ) { }
+    
+    @Get()
+    getAll() {
+        return this.categoriesService.getAll()
+    }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ICategoryService } from 'src/types/services/ICategoryService';
+import { Repository } from 'typeorm';
+import { Category } from './categories.entity';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService implements ICategoryService {
+    constructor(@InjectRepository(Category) private repo: Repository<Category>){}
+
+    getAll() {
+        return this.repo.find();
+    }
+}
